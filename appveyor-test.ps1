fcdd74481da69daea748e01aa44380e6b1891491
itn3000 <itn3000@gmail.com>
2017-05-31 09:45:04 +0900

    add debug

--- appveyor-test.ps1
+++ appveyor-test.ps1
@@ -1,5 +1,5 @@
 $natsexe = Resolve-Path -Path $env:NATS_EXECUTABLE
-Get-ChildItem
+Get-ChildItem -Path $(Join-Path $(Get-Location) "gnatsd-v0.9.6-windows-386")
 $args = @("-a","127.0.0.1","-p","4222")
 
 $ErrorActionPreference = "Stop"
