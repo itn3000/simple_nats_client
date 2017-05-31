1de9f0e51148cab5c8056acc867aba870167c0d7
itn3000 <itn3000@gmail.com>
2017-05-31 09:09:37 +0900

    create appveyor setting files

--- appveyor-test.ps1
+++ appveyor-test.ps1
@@ -1,11 +1,14 @@
-$natsexe = $env:NATS_EXECUTABLE
+$natsexe = Resolve-Path -Path $env:NATS_EXECUTABLE
 $args = @("-a","127.0.0.1","-p","4222")
 
 $ErrorActionPreference = "Stop"
 
 $natstask = Start-Process -FilePath $natsexe -ArgumentList $args -NoNewWindow -PassThru
 
+
 cargo test --verbose
+$er = $LASTEXITCODE
 
 $natstask.Kill()
 
+exit $er
